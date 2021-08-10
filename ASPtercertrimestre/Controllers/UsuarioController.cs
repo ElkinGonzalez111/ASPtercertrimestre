c91d8074b99948b59713d5ca6a336f1e66fc8516
Elkin David Gonzalez Suarez <eldagozu@hotmail.com>
2021-08-10 12:41:15 -0500

    terminando el crud de usuarios

--- ASPtercertrimestre/Controllers/UsuarioController.cs
+++ ASPtercertrimestre/Controllers/UsuarioController.cs
@@ -46,5 +46,82 @@ namespace ASPtercertrimestre.Controllers
             }
 
         }
+
+        public ActionResult Details(int id)
+        { 
+            using (var db = new inventarioo2021Entities())
+            {
+               var findUser = db.usuario.Find(id);
+               return View(findUser);
+            }
+
+        }
+
+        public ActionResult Delete(int id)
+
+
+        {
+            try
+            {
+                using (var db = new inventarioo2021Entities())
+                {
+                    var finUser = db.usuario.Find(id);
+                    db.usuario.Remove(finUser);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+
+
+        {
+            try
+            {
+                using (var db = new inventarioo2021Entities())
+                {
+                    usuario findUser = db.usuario.Where(a => a.id == id).FirstOrDefault();
+                    return View(findUser);
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(usuario editUser)
+        {
+            try
+            {
+                using (var db = new inventarioo2021Entities())
+                {
+                    usuario user = db.usuario.Find(editUser.id);
+
+                    user.nombre = editUser.nombre;
+                    user.apellido = editUser.apellido;
+                    user.email = editUser.email;
+                    user.fecha_nacimiento = editUser.fecha_nacimiento;
+                    user.password = editUser.password;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+
+                }
+            }catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
     }
 }
