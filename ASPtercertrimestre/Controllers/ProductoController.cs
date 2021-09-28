dbf85d186a2e569a35da930eab95a94388fda08e
Elkin David Gonzalez Suarez <eldagozu@hotmail.com>
2021-09-28 09:40:43 -0500

    Cargando y mostrando imagen al producto

--- ASPtercertrimestre/Controllers/ProductoController.cs
+++ ASPtercertrimestre/Controllers/ProductoController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using ASPtercertrimestre.Models;
 using Rotativa;
 
+
 namespace ASPtercertrimestre.Controllers
 {
     public class ProductoController : Controller
--- ASPtercertrimestre/Controllers/ProductoimagenController.cs
+++ ASPtercertrimestre/Controllers/ProductoimagenController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 using ASPtercertrimestre.Models;
 
 namespace ASPtercertrimestre.Controllers
@@ -129,6 +130,65 @@ namespace ASPtercertrimestre.Controllers
                 ModelState.AddModelError("", "error" + ex);
                 return View();
             }
+        }
+        public ActionResult CargarImagen()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CargarImagen(int id_producto, HttpPostedFileBase imagen)
+        {
+            try
+            {
+                //string para guardar la ruta
+                string filePath = string.Empty;
+                string nameFile = "";
+
+                //condicion para saber si el archivo llego
+                if (imagen != null)
+                {
+                    //ruta de la carpeta que guardara el archivo
+                    string path = Server.MapPath("~/Uploads/Imagenes/");
+
+                    //condicion para saber si la carpeta uploads existe
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    nameFile = Path.GetFileName(imagen.FileName);
+
+                    //obtener el nombre del archivo
+                    filePath = path + Path.GetFileName(imagen.FileName);
+
+                    //obtener la extension del archivo
+                    string extension = Path.GetExtension(imagen.FileName);
+
+                    //guardar el archivo
+                    imagen.SaveAs(filePath);
+                }
+
+                using (var db = new inventarioo2021Entities())
+                {
+                    var imagenProducto = new producto_imagen();
+                    imagenProducto.id_producto = id_producto;
+                    imagenProducto.imagen = "/Uploads/Imagenes/" + nameFile;
+                    db.producto_imagen.Add(imagenProducto);
+                    db.SaveChanges();
+
+                }
+
+                return View();
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+
         }
     }
 }
